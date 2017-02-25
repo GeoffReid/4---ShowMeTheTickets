74a65950543f4fe5d445a85b2a6fdc80be9c1de6
Geoff <Geoff@GPC>
2017-02-25 18:09:58 +0000

    Worked around permission error, can return name of event in category.

--- 4 - ShowMeTheTickets/Controllers/HomeController.cs
+++ 4 - ShowMeTheTickets/Controllers/HomeController.cs
@@ -1,13 +1,4 @@
-﻿/*using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using GogoKit;
-*/
-
-
-using GogoKit;
+﻿using GogoKit;
 using GogoKit.Enumerations;
 using GogoKit.Models.Response;
 using System;
@@ -29,70 +20,51 @@ namespace _4___ShowMeTheTickets.Controllers
 
         private static readonly string[] Scopes = { "(no scope)" };
 
-        private static readonly Uri RedirectUri = new Uri("https://localhost:50442/yo");
+        private static readonly Uri RedirectUri = new Uri("https://localhost:50442/getEvents");
 
         public HomeController()
         {
-            Debug.WriteLine("My debug string here\n");
+            Debug.WriteLine("Start\n");
 
             _viagogoClient = new ViagogoClient(new ProductHeaderValue("ShowMeTheTickets"), CLIENT_ID,
             CLIENT_SECRET);
 
             getEvents();
-
-
-
         }
 
+        public ActionResult Index()
+        {
+
+            return View();
+        }
+
+        /* Aware that this shouldn't be a void. At the moment the only way I seem to be able to return a
+         category without getting a permission error is by doing it this way*/
         public async void getEvents()
         {
+            Debug.WriteLine("Getting client access token\n");
             var token = await _viagogoClient.OAuth2.GetClientAccessTokenAsync(Scopes);
             await _viagogoClient.TokenStore.SetTokenAsync(token);
             Debug.WriteLine(token);
 
+            Debug.WriteLine("Getting all events in category\n");
             var events = await _viagogoClient.Events.GetAllByCategoryAsync(1207);
             var eventName = events[0].Name;
             
-            Debug.WriteLine("Event " + eventName + "\n");
-
-
-            /*foreach (object o in searchResults)
-            {
-                Debug.WriteLine(o);
-
-            }*/
-
-
-
+            Debug.WriteLine("Event 1 title: " + eventName + "\n");
         }
-
-        [Route("")]
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-
-
-        /*[Route("hello")]
-        public async Task<ActionResult> hello(string code, string state)
-        {
-            Debug.WriteLine("hit\n");
-            var token = await _viagogoClient.OAuth2.GetAuthorizationCodeAccessTokenAsync(code, RedirectUri, Scopes);
-
-            // Save the token for making API requests later. 
-            await _viagogoClient.TokenStore.SetTokenAsync(token);
-
-            var searchResults = await _viagogoClient.Search.GetAllAsync("lady gaga");
-
-            return Content(token.AccessToken);
-        }*/
-
-
-
 
 
         
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ 4 - ShowMeTheTickets/Models/EventModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _4___ShowMeTheTickets.Models
+{
+    public class EventModel
+    {
+        public string eventName { get; set; }
+    }
+}
