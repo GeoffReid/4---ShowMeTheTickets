3c98bc7ae1de41518cde65d22ccb309b3c6db276
Geoff <Geoff@GPC>
2017-02-25 15:24:50 +0000

    Successful retrieval of client access token and name of first event in category 1207

--- 4 - ShowMeTheTickets/Controllers/HomeController.cs
+++ 4 - ShowMeTheTickets/Controllers/HomeController.cs
@@ -1,30 +1,100 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using GogoKit;
+*/
+
+
+using GogoKit;
+using GogoKit.Enumerations;
+using GogoKit.Models.Response;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
 
 namespace _4___ShowMeTheTickets.Controllers
 {
     public class HomeController : Controller
     {
+        private const string CLIENT_ID = "TaRJnBcw1ZvYOXENCtj5";
+        private const string CLIENT_SECRET = "ixGDUqRA5coOHf3FQysjd704BPptwbk6zZreELW2aCYSmIT8XJ9ngvN1MuKV";
+        private readonly IViagogoClient _viagogoClient;
+
+        private static readonly string[] Scopes = { "(no scope)" };
+
+        private static readonly Uri RedirectUri = new Uri("https://localhost:50442/yo");
+
+        public HomeController()
+        {
+            Debug.WriteLine("My debug string here\n");
+
+            _viagogoClient = new ViagogoClient(new ProductHeaderValue("ShowMeTheTickets"), CLIENT_ID,
+            CLIENT_SECRET);
+
+            getEvents();
+
+
+
+        }
+
+        public async void getEvents()
+        {
+            var token = await _viagogoClient.OAuth2.GetClientAccessTokenAsync(Scopes);
+            await _viagogoClient.TokenStore.SetTokenAsync(token);
+            Debug.WriteLine(token);
+
+            var events = await _viagogoClient.Events.GetAllByCategoryAsync(1207);
+            var eventName = events[0].Name;
+            
+            Debug.WriteLine("Event " + eventName + "\n");
+
+
+            /*foreach (object o in searchResults)
+            {
+                Debug.WriteLine(o);
+
+            }*/
+
+
+
+        }
+
+        [Route("")]
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+
+
+        /*[Route("hello")]
+        public async Task<ActionResult> hello(string code, string state)
         {
-            ViewBag.Message = "Your application description page.";
+            Debug.WriteLine("hit\n");
+            var token = await _viagogoClient.OAuth2.GetAuthorizationCodeAccessTokenAsync(code, RedirectUri, Scopes);
 
-            return View();
-        }
+            // Save the token for making API requests later. 
+            await _viagogoClient.TokenStore.SetTokenAsync(token);
 
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
+            var searchResults = await _viagogoClient.Search.GetAllAsync("lady gaga");
 
-            return View();
-        }
+            return Content(token.AccessToken);
+        }*/
+
+
+
+
+
+        
+
+
+
     }
 }
